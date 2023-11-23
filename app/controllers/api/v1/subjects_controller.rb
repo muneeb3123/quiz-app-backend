1e1e793a932a6e86d2c0a0a17aa870461d848897
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-11-23 21:16:10 +0500

    create index show create and destroy actions

--- app/controllers/api/v1/subjects_controller.rb
+++ app/controllers/api/v1/subjects_controller.rb
@@ -4,4 +4,30 @@ class Api::V1::SubjectsController < ApplicationController
         @subjects = Subject.all
         render json: @subjects
     end
+
+    def show
+        @subject = Subject.find(params[:id])
+        render json: {message: "hy"}
+    end
+
+    def create
+        @subject = Subject.new(subject_params)
+        if @subject.save
+            render json: {status: 'SUCCESS', message: 'Saved subject', data: @subject}, status: :ok
+        else
+            render json: {message: 'Subject not saved', data: @subject.errors}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @subject = Subject.find(params[:id])
+        @subject.destroy
+        render json: {message: 'Subject deleted'}, status: :ok
+    end
+end
+
+private
+
+def subject_params
+    params.require(:subject).permit(:sub_name, :sub_img);
 end
