90ac4c77ccb503f4572a5b5920cda2792069ccf0
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-11-24 20:49:51 +0500

    write routes for quiz actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subjects, only: [:index, :show, :new, :create, :destroy]
+      resources :quizzes, only: [:index, :show, :new, :create, :destroy]
   end
 end
 end
