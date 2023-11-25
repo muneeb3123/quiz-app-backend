9c453cdcfe13b089fda74ad2fc99a58c23af7f2b
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-11-25 20:44:55 +0500

    create routes for options actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :subjects, only: [:index, :show, :new, :create, :destroy]
       resources :quizzes, only: [:index, :show, :new, :create, :destroy]
+      resources :options, only: [:index, :show, :new, :create, :destroy]
   end
 end
 end
