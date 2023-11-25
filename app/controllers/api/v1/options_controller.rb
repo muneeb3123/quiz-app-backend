0cb58ab1071c927c0dd46c3037fede8d28d0f388
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-11-25 20:43:38 +0500

    create actions for option controller

--- app/controllers/api/v1/options_controller.rb
+++ app/controllers/api/v1/options_controller.rb
@@ -4,4 +4,29 @@ class Api::V1::OptionsController < ApplicationController
         @options = Option.all
         render json: @options
     end
+
+    def show
+        @option = Option.find(params[:id])
+        render json: @option
+    end
+
+    def create
+        @option = Option.new(option_params)
+        if @option.save
+            render json: @option
+        else
+            render json: {error: 'Error creating option'}
+        end
+    end
+
+    def destroy
+        @option = Option.find(params[:id])
+        @option.destroy
+    end
+
+    private
+
+    def option_params
+        params.require(:option).permit(:value1, :value2, :value3, :value4, :quiz_id)
+    end
 end
